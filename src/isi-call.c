802caf55a50fe07efd6f6aec2286263779c559e8
Klaus Kurzmann <mok@fluxnetz.de>
2011-04-08 22:32:32 +0200

    isi-call: implement all known call commands with col_set_str
    Signed-off-by: Klaus Kurzmann <mok@fluxnetz.de>

--- src/isi-call.c
+++ src/isi-call.c
@@ -151,6 +151,250 @@ static void dissect_isi_call(tvbuff_t *tvb, packet_info *pinfo, proto_item *isit
 		cmd = tvb_get_guint8(tvb, 0);
 
 		switch (cmd) {
+			case 0x01: /* CALL_CREATE_REQ */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Create Request");
+				break;
+			case 0x02: /* CALL_CREATE_RESP */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Create Response");
+				break;
+			case 0x03: /* CALL_COMING_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Coming Indication");
+				break;
+			case 0x04: /* CALL_MO_ALERT_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call MO Allert Indication");
+				break;
+			case 0x05: /* CALL_MT_ALERT_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call MT Allert Indication");
+				break;
+			case 0x06: /* CALL_WAITING_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Waiting Indication");
+				break;
+			case 0x07: /* CALL_ANSWER_REQ */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Answer Request");
+				break;
+			case 0x08: /* CALL_ANSWER_RESP */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Answer Response");
+				break;
+			case 0x09: /* CALL_RELEASE_REQ */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Release Request");
+				break;
+			case 0x0A: /* CALL_RELEASE_RESP */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Release Response");
+				break;
+			case 0x0B: /* CALL_RELEASE_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Release Indication");
+				break;
+			case 0x0C: /* CALL_TERMINATED_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Terminated Indication");
+				break;
+			case 0x0D: /* CALL_STATUS_REQ */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Status Request");
+				break;
+			case 0x0E: /* CALL_STATUS_RESP */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Status Response");
+				break;
+			case 0x0F: /* CALL_STATUS_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Status Indication");
+				break;
+			case 0x10: /* CALL_SERVER_STATUS_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Server Status Indication");
+				break;
+			case 0x11: /* CALL_CONTROL_REQ */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Control Request");
+				break;
+			case 0x12: /* CALL_CONTROL_RESP */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Control Response");
+				break;
+			case 0x13: /* CALL_CONTROL_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Control Indication");
+				break;
+			case 0x14: /* CALL_MODE_SWITCH_REQ */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Mode Switch Request");
+				break;
+			case 0x15: /* CALL_MODE_SWITCH_RESP */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Mode Switch Response");
+				break;
+			case 0x16: /* CALL_MODE_SWITCH_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Mode Switch Indication");
+				break;
+			case 0x17: /* CALL_DTMF_SEND_REQ */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call DTMF Send Request");
+				break;
+			case 0x18: /* CALL_DTMF_SEND_RESP */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call DTMF Send Response");
+				break;
+			case 0x19: /* CALL_DTMF_STOP_REQ */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call DTMF Stop Request");
+				break;
+			case 0x1A: /* CALL_DTMF_STOP_RESP */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call DTMF Stop Response");
+				break;
+			case 0x1B: /* CALL_DTMF_STATUS_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call DTMF Status Indication");
+				break;
+			case 0x1C: /* CALL_DTMF_TONE_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call DTMF Tone Indication");
+				break;
+			case 0x1E: /* CALL_RECONNECT_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Reconnect Indication");
+				break;
+			case 0x1F: /* CALL_PROPERTY_GET_REQ */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Property Get Request");
+				break;
+			case 0x20: /* CALL_PROPERTY_GET_RESP */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Property Get Response");
+				break;
+			case 0x21: /* CALL_PROPERTY_SET_REQ */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Property Set Request");
+				break;
+			case 0x22: /* CALL_PROPERTY_SET_RESP */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Property Set Response");
+				break;
+			case 0x23: /* CALL_PROPERTY_SET_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Property Set Indication");
+				break;
+			case 0x28: /* CALL_EMERGENCY_NBR_CHECK_REQ */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Emergency NBR Check Request");
+				break;
+			case 0x29: /* CALL_EMERGENCY_NBR_CHECK_RESP */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Emergency NBR Check Response");
+				break;
+			case 0x26: /* CALL_EMERGENCY_NBR_GET_REQ */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Emergency NBR Get Request");
+				break;
+			case 0x27: /* CALL_EMERGENCY_NBR_GET_RESP */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Emergency NBR Get Response");
+				break;
+			case 0x24: /* CALL_EMERGENCY_NBR_MODIFY_REQ */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Emergency NBR Modify Request");
+				break;
+			case 0x25: /* CALL_EMERGENCY_NBR_MODIFY_RESP */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Emergency NBR Modify Response");
+				break;
+			case 0xA0: /* CALL_GSM_NOTIFICATION_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call GSM Notification Indication");
+				break;
+			case 0xA1: /* CALL_GSM_USER_TO_USER_REQ */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call GSM User To User Request");
+				break;
+			case 0xA2: /* CALL_GSM_USER_TO_USER_RESP */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call GSM User To User Response");
+				break;
+			case 0xA3: /* CALL_GSM_USER_TO_USER_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call GSM User To User Indication");
+				break;
+			case 0xA4: /* CALL_GSM_BLACKLIST_CLEAR_REQ */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call GSM Blacklist Clear Request");
+				break;
+			case 0xA5: /* CALL_GSM_BLACKLIST_CLEAR_RESP */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call GSM Blacklist Clear Response");
+				break;
+			case 0xA6: /* CALL_GSM_BLACKLIST_TIMER_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call GSM Blacklist Timer Indication");
+				break;
+			case 0xA7: /* CALL_GSM_DATA_CH_INFO_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call GSM Data Channel Info Indication");
+				break;
+			case 0xAA: /* CALL_GSM_CCP_GET_REQ */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call GSM CCP Get Request");
+				break;
+			case 0xAB: /* CALL_GSM_CCP_GET_RESP */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call GSM CCP Get Response");
+				break;
+			case 0xAC: /* CALL_GSM_CCP_CHECK_REQ */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call GSM CCP Check Request");
+				break;
+			case 0xAD: /* CALL_GSM_CCP_CHECK_RESP */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call GSM CCP Check Response");
+				break;
+			case 0xA9: /* CALL_GSM_COMING_REJ_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call GSM Coming Reject Indication");
+				break;
+			case 0xA8: /* CALL_GSM_RAB_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call GSM RAB Indication");
+				break;
+			case 0xAE: /* CALL_GSM_IMMEDIATE_MODIFY_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call GSM Immediate Modify Indication");
+				break;
+			case 0x2A: /* CALL_CREATE_NO_SIMATK_REQ */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Create No SIMATK Request");
+				break;
+			case 0xAF: /* CALL_GSM_SS_DATA_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call GSM SS Data Indication");
+				break;
+			case 0x2B: /* CALL_TIMER_REQ */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Timer Request");
+				break;
+			case 0x2C: /* CALL_TIMER_RESP */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Timer Response");
+				break;
+			case 0x2D: /* CALL_TIMER_NTF */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Timer NTF");
+				break;
+			case 0x2E: /* CALL_TIMER_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Timer Indication");
+				break;
+			case 0x2F: /* CALL_TIMER_RESET_REQ */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Timer Reset Request");
+				break;
+			case 0x30: /* CALL_TIMER_RESET_RESP */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Timer Reset Response");
+				break;
+			case 0x31: /* CALL_EMERGENCY_NBR_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Emergency NBR Indication");
+				break;
+			case 0x32: /* CALL_SERVICE_DENIED_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Service Denied Indication");
+				break;
+			case 0x34: /* CALL_RELEASE_END_REQ */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Release End Request");
+				break;
+			case 0x35: /* CALL_RELEASE_END_RESP */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Release End Response");
+				break;
+			case 0x33: /* CALL_USER_CONNECT_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call User Connect Indication");
+				break;
+			case 0x40: /* CALL_AUDIO_CONNECT_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Audio Connect Indication");
+				break;
+			case 0x36: /* CALL_KODIAK_ALLOW_CTRL_REQ */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Kodiak Allow Control Request");
+				break;
+			case 0x37: /* CALL_KODIAK_ALLOW_CTRL_RESP */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Kodiak Allow Control Response");
+				break;
+			case 0x38: /* CALL_SERVICE_ACTIVATE_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Service Activate Indication");
+				break;
+			case 0x39: /* CALL_SERVICE_ACTIVATE_REQ */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Service Activate Request");
+				break;
+			case 0x3A: /* CALL_SERVICE_ACTIVATE_RESP */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Service Activate Response");
+				break;
+			case 0x3B: /* CALL_SIM_ATK_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call SIM ATK Indication");
+				break;
+			case 0x3C: /* CALL_CONTROL_OPER_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Control Operator Indication");
+				break;
+			case 0x3E: /* CALL_TEST_CALL_STATUS_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Test Call Status Indication");
+				break;
+			case 0x3F: /* CALL_SIM_ATK_INFO_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call SIM ATK Info Indication");
+				break;
+			case 0x41: /* CALL_SECURITY_IND */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Security Indication");
+				break;
+			case 0x42: /* CALL_MEDIA_HANDLE_REQ */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Media Handle Request");
+				break;
+			case 0x43: /* CALL_MEDIA_HANDLE_RESP */
+				col_set_str(pinfo->cinfo, COL_INFO, "Call Media Handle Response");
+				break;
+
 			case 0xF0: /* CALL_COMMON_MESSAGE */
 				dissect_isi_common("Call", tvb, pinfo, tree);
 				break;
